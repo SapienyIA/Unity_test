bcef66f838ea50dffc4bd0ded95bd36cf48a10ad
U-PC40911\USER <iasapieny@gmail.com>
2023-07-05 13:10:48 +0800

    Update slimer's action

--- Assets/Scenes/A.cs
+++ Assets/Scenes/A.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class A : MonoBehaviour
 {
-    int lv = 100;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
-
-    // Update is called once per frame
-    void Update()
+    public static int flag = 0;
+// Update is called once per frame
+void Update()
     {
-        
+    Vector3 slimer = this.GetComponent<Transform>().position;
+    if (slimer.x < 6f && flag == 0) { transform.Translate(-0.05f, 0, 0); }
+    if (slimer.x <= -6f) { 
+            flag =1;
+            transform.Translate(0.05f, 0, 0);
+        }
+    if (slimer.x >= -6f && flag ==1) { transform.Translate(0.05f, 0, 0); }
+    if (slimer.x > 6f ) { 
+            flag = 0;
+            transform.Translate(-0.05f, 0, 0);
+        }
     }
 }
